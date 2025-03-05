e70048f2e1442f63a4610fdfeb6a20e7b06f1431
FireFang_School <dcentov@guardiaschool.fr>
2025-03-05 12:41:52 +0100

    5555

--- UE6-3_Algo_Chifrement/client.py
+++ UE6-3_Algo_Chifrement/client.py
@@ -9,7 +9,7 @@ P = 23
 G = 5
 
 class SecureChatClient:
-    def __init__(self, host="127.0.0.1", port=12345):
+    def __init__(self, host="127.0.0.1", port=5555):
         self.host = host
         self.port = port
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
